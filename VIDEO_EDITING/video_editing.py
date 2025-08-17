0a14f4f7ba1c7ff11a8c802c485ac65d1934a460
CaughtYouSmiling <wemadeyouusmile@gmail.com>
2025-08-17 20:59:45 +0530

    updated video editing script to include new height calculation

--- VIDEO_EDITING/video_editing.py
+++ VIDEO_EDITING/video_editing.py
@@ -366,13 +366,13 @@ def process_video(input_video_path, reel_number):
 
             temp_width = new_width
             temp_height = new_height - 200
-            new_width = temp_width
-            new_height = temp_height
             if new_width <= 0:
                 new_width = temp_width
             if temp_height <= 1200:
                 new_height = new_height
-            
+            else:
+                new_height = temp_height
+
             # print("new_width = ",new_width)
             # print("new_height = ",new_height)
             
